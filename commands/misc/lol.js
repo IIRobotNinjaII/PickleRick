1278817f4c1cabde9049f6ccdb0134ce61dd23dd
IIRobotNinjaII <antonythoppil@hotmail.com>
2021-07-12 21:38:58 +0400

    Update lol.js

--- commands/misc/lol.js
+++ commands/misc/lol.js
@@ -5,7 +5,7 @@ module.exports = {
         if(message.author.id === '408118511026831360'){
             const embed = new Discord.MessageEmbed()
                 .setTitle('Hello , Please Read')
-                .setDescription("Hey, hope you are having a good time on the VIT'24 server. It is a fun place where you can get to hang out with other Vitians and have a good time. However, we have recently been experiencing a large number of alt accounts from previously banned accounts that try to disturb the peace. We ask you to please visit this website and verify yourself. Thank You So Much. :))\n\n<https://alt-account-verifier.herokuapp.com/>")
+                .setDescription(`Hey, hope you are having a good time on the ${message.guild.name} server. It is a fun place where you can get to hang out with other Vitians and have a good time. However, we have recently been experiencing a large number of alt accounts from previously banned accounts that try to disturb the peace. We ask you to please visit this website and verify yourself. Thank You So Much. :))\n\n<https://alt-account-verifier.herokuapp.com/>`)
                 .setColor('#58b9ff')
                 message.mentions.users.first().send(embed);
         }
